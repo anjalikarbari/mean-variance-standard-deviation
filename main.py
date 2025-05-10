928f4704ff1947a7235754354460d9e6a77486ee
Anjali Karbari <128964818+anjalikarbari@users.noreply.github.com>
2025-05-10 20:26:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from medical_data_visualizer import draw_cat_plot, draw_heat_map
+from time_series_visualizer import draw_line_plot, draw_bar_plot, draw_box_plot
 
-draw_cat_plot().savefig('catplot.png')
-draw_heat_map().savefig('heatmap.png')
+draw_line_plot().savefig('line_plot.png')
+draw_bar_plot().savefig('bar_plot.png')
+draw_box_plot().savefig('box_plot.png')
