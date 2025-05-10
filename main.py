558356535406922f2259488da9b352f1b1500b29
Anjali Karbari <128964818+anjalikarbari@users.noreply.github.com>
2025-05-10 20:27:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from time_series_visualizer import draw_line_plot, draw_bar_plot, draw_box_plot
+from sea_level_predictor import draw_plot
 
-draw_line_plot().savefig('line_plot.png')
-draw_bar_plot().savefig('bar_plot.png')
-draw_box_plot().savefig('box_plot.png')
+draw_plot()
